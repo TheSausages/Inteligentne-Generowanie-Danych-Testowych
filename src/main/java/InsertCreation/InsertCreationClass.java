c6d57d053f6966996b91ee17f0f1349c65cfe8a7
Korowojow <kacper.top@interia.pl>
2021-03-31 18:14:38 +0200

    Developed InsertCreationClass

--- src/main/java/InsertCreation/InsertCreationClass.java
+++ src/main/java/InsertCreation/InsertCreationClass.java
@@ -1,17 +1,24 @@
 package InsertCreation;
 import java.io.*;
-import java.sql.SQLData;
 import java.util.ArrayList;
 import java.util.List;
 
 import DatabaseConnection.SupportedDatabases;
 import TableMapping.ColumnMappingClass;
-import TableMapping.Field;
-import TableMapping.ForeignKeyMapping;
+
 import TableMapping.TableMappingClass;
-import jdk.jfr.Unsigned;
+
 
 public class InsertCreationClass {
+    public static String removeLastCharacter(String str) {
+        String result = null;
+        if ((str != null) && (str.length() > 0)) {
+            result = str.substring(0, str.length() - 1);
+        }
+        return result;
+    }
+
+
     public void InsertCreationClass() {
 
         List<TableMappingClass> mappedTables = new ArrayList<>();
@@ -32,7 +39,6 @@ public class InsertCreationClass {
         pierwsza.addColumn(numer);
         mappedTables.add(pierwsza);
 
-
         TableMappingClass druga =  new TableMappingClass();
         druga.setTableName("Baza2");
         druga.setTableType(SupportedDatabases.MYSQL);
@@ -42,72 +48,45 @@ public class InsertCreationClass {
         druga.addColumn(numer);
         mappedTables.add(druga);
 
+        mappedTables.forEach(tableMappingClass -> {
+            System.out.println("INSERT INTO ");
+            System.out.printf(tableMappingClass.getTableName());
+            System.out.printf(" (");
+            tableMappingClass.getColumns().forEach(columnMappingClass -> {
+                        System.out.printf(columnMappingClass.getName());
+                        System.out.printf(",");
 
+                    });
+            System.out.println(")");
+            System.out.printf("VALUES ('");
+            System.out.println("....','....','....','....')");
+        });
 
-//        mappedTables.forEach(tableMappingClass -> {
-//            System.out.println("INSERT INTO ");
-//            System.out.printf(tableMappingClass.getTableName());
-//            System.out.printf(" (");
-//            tableMappingClass.getColumns().forEach(columnMappingClass -> {
-//                        System.out.printf(columnMappingClass.getName());
-//                        System.out.printf(",");
-//                    });
-//            System.out.println(")");
-//            System.out.printf("VALUES ('");
-//            System.out.println("....','....','....','....')");
-//        });
-
-
+        var ref = new Object() {
+            String str1="";
+            String str2="";
+            String str3="";
+        };
 
         try {
             File newTextFile = new File("C:/thetextfile.txt");
             FileWriter fw = new FileWriter(newTextFile);
         mappedTables.forEach(tableMappingClass -> {
-                    String str1 = "INSERT INTO " + tableMappingClass.getTableName() + " (";
-            try {
-                fw.write(str1);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+                    ref.str1 = "INSERT INTO " + tableMappingClass.getTableName() + " (";
             tableMappingClass.getColumns().forEach(columnMappingClass -> {
-                    String str2 = columnMappingClass.getName()+",";
-                try {
-                    fw.write(str2);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-
+                    ref.str2 = ref.str2 + columnMappingClass.getName()+",";
             });
-                 String str3 = ") VALUES ('....','....','....','....');";
+            ref.str3 = ") VALUES ('....','....','....','....');";
             try {
-                fw.write(str3);
+                fw.write(ref.str1+removeLastCharacter(ref.str2)+ ref.str3);
             } catch (IOException e) {
                 e.printStackTrace();
             }
+            ref.str2="";
         });
             fw.close();
-
-       } catch (IOException iox) {
+        } catch (IOException iox) {
             iox.printStackTrace();
         }
-
-//        String str1 = "INSERT INTO table_name ("+jeden.getName()+","+dwa.getName()+","+trzy.getName()+","+cztery.getName()+") VALUES ('"+one.getNamee()+"','"+one.getSurname()+"','"+one.getCar()+"','"+one.getNumber()+"');";
-//        String str2 = "INSERT INTO table_name ("+jeden.getName()+","+dwa.getName()+","+trzy.getName()+","+cztery.getName()+") VALUES ('"+two.getNamee()+"','"+two.getSurname()+"','"+two.getCar()+"','"+two.getNumber()+"');";
-//
-
-
-//        try {
-//            File newTextFile = new File("C:/thetextfile.txt");
-//            FileWriter fw = new FileWriter(newTextFile);
-//            fw.write(st);
-//            fw.close();
-//
-//        } catch (IOException iox) {
-//            iox.printStackTrace();
-//        }
-
-        //INSERT INTO table_name (column1, column2, column3, ...)
-        //VALUES (value1, value2, value3, ...);
-        //System.out.printf("insert");
     }
 }
