2bb2ac03c6cbc5d04493e38c8c4c604030772e26
Korowojow <kacper.top@interia.pl>
2021-03-31 10:22:00 +0200

    Developed InsertCreationClass

--- src/main/java/InsertCreation/Data.java
+++ src/main/java/InsertCreation/Data.java
@@ -1,10 +1,38 @@
 package InsertCreation;
 
 public class Data {
-    String name;
-    String surname;
-    String car;
-    Integer number;
+    private String name;
+    public void setNamee(String name) {
+        this.name = name;
+    }
+    public String getNamee() {
+        return name;
+    }
+
+    private String surname;
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+    public String getSurname() {
+        return surname;
+    }
+
+    private String car;
+    public void setCar(String car) {
+        this.car = car;
+    }
+    public String getCar() {
+        return car;
+    }
+
+    private Integer number;
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+    public Integer getNumber() {
+        return number;
+    }
+
     String wypisz() {
         String str = "INSERT INTO table_name (name, surname, car, number) VALUES ('"+name+"','"+surname+"','"+car+"','"+number+"');";
         return str;
--- src/main/java/InsertCreation/InsertCreationClass.java
+++ src/main/java/InsertCreation/InsertCreationClass.java
@@ -1,22 +1,43 @@
 package InsertCreation;
 import java.io.*;
-
+import TableMapping.ColumnMappingClass;
+import TableMapping.ForeignKeyMapping;
 public class InsertCreationClass {
     public static void InsertCreationClass(String[] args) {
 
-        Data one = new Data();
-        one.name = "Piotr";
-        one.surname = "Kiszewski";
-        one.car = "Volvo";
-        one.number = 999;
+    Data one = new Data();
+        one.setNamee("Piotr");
+        one.setSurname("Kiszowski");
+        one.setCar("Volvo");
+        one.setNumber(999);
 
         Data two= new Data();
-        two.name = "Kacper";
-        two.surname = "Morelowski";
-        two.car = "Suzuki";
-        two.number = 778;
+        two.setNamee("Kacper");
+        two.setSurname("Morelowski");
+        two.setCar("Suzuki");
+        two.setNumber(778);
 
-        String st = one.wypisz() + two.wypisz();
+        ColumnMappingClass jeden = new ColumnMappingClass();
+        ColumnMappingClass dwa = new ColumnMappingClass();
+        ColumnMappingClass trzy = new ColumnMappingClass();
+        ColumnMappingClass cztery = new ColumnMappingClass();
+        jeden.setName("name");
+        dwa.setName("surname");
+        trzy.setName("car");
+        cztery.setName("number");
+        jeden.setAutoIncrement(false);
+        dwa.setAutoIncrement(false);
+        trzy.setAutoIncrement(false);
+        cztery.setAutoIncrement(true);
+        jeden.setDefaultValue("");
+        dwa.setDefaultValue("");
+        trzy.setDefaultValue("");
+        cztery.setDefaultValue("");
+
+        String str1 = "INSERT INTO table_name ("+jeden.getName()+","+dwa.getName()+","+trzy.getName()+","+cztery.getName()+") VALUES ('"+one.getNamee()+"','"+one.getSurname()+"','"+one.getCar()+"','"+one.getNumber()+"');";
+        String str2 = "INSERT INTO table_name ("+jeden.getName()+","+dwa.getName()+","+trzy.getName()+","+cztery.getName()+") VALUES ('"+two.getNamee()+"','"+two.getSurname()+"','"+two.getCar()+"','"+two.getNumber()+"');";
+
+        String st = str1 + str2;
 
         try {
             File newTextFile = new File("C:/thetextfile.txt");
