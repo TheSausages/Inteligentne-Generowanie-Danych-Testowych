ef54a84fc7701f0a5bd12c02b392abe34ccfedf9
Korowojow <kacper.top@interia.pl>
2021-04-01 13:29:02 +0200

    Developed InsertCreationClass

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,17 @@
+import DatabaseConnection.SupportedDatabases;
 import Gui.MainGui;
 import InsertCreation.InsertCreationClass;
+import TableMapping.ColumnMappingClass;
+import TableMapping.TableMappingClass;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static javafx.application.Application.launch;
 
 public class Main {
     public static void main(String[] args)  {
-        //launch(MainGui.class, args);
-        new InsertCreationClass().InsertCreationClass();
+        launch(MainGui.class, args);
 
         /*
             Oracle: localhost, 1521, xe, system, system
